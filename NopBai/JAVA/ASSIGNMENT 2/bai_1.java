ff2b7ca12da3c0a4e36d4988d4f8364652b255dd
anhvit10 <menbinh74@gmail.com>
2020-01-14 00:07:05 +0700

    complete ass 2

---
+++ NopBai/JAVA/ASSIGNMENT 2/bai_1.java
@@ -0,0 +1,52 @@
+package assignment_1;
+
+import java.util.Random;
+
+public class bai_1 {
+
+	public static void main(String[] args) {
+		
+		String[] FirstNameArray = {"Nguyen ", "Tran ", "Pham ", "Vu ", "Ha ", "Bui ", "Ngo "};
+		
+		String[] MiddleNameArray = {"Viet ", "Thi ", "Duy ", "Van ", "Thu ", "Tien ", "Trung "};
+				
+		String[] LastNameArray = {"Anh	", "Trang	", "Thang	", "Thanh	", "Tra	", "Dung	", "Nhat	"};
+		
+// question 1		
+		String[] Name = new String[10];
+		Random rd = new Random();
+			for(int i=0;i<=9;i++)
+				{
+					int x = rd.nextInt(7);
+					int y = rd.nextInt(7);
+					int z = rd.nextInt(7);
+					Name[i] = FirstNameArray[x] + MiddleNameArray[y] + LastNameArray[z];
+				}
+			
+//question 2	
+		String[] ID = new String[10];
+		int id = 1111111;
+			for(int i=0;i<=9;i++)
+				{
+					ID[i] = "SV" + id +"     ";
+					id++;
+				}
+		
+// question 3
+		String[] BirthDate = new String[10];
+			for(int i=0;i<=9;i++)
+				{
+					int x = 1 + rd.nextInt(31);
+					int y = 1 + rd.nextInt(12);
+					BirthDate[i] = x + "/" + y + "/" + "1991";
+				}
+			
+// question 4 
+		System.out.println("Full Name       " + "ID            " + "Date");
+		System.out.println("-------------------------------------------");
+			for(int i=0;i<=9;i++)
+				{
+					System.out.println(Name[i] + ID[i] + BirthDate[i]);
+				}			
+	}
+}
